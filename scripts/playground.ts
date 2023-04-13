b9c7462eac70043f6f4e7bbb3a302517cdce0eb2
Nik <nik@niktek.io>
2023-04-13 16:53:27 +1000

    Cleanup

--- scripts/playground.ts
+++
@@ -1,5 +0,0 @@
-import { restoreData } from './datastore';
-
-console.log(__dirname)
-const foo = restoreData();
-console.dir(foo);
--- scripts/query.ts
+++
@@ -1,5 +0,0 @@
-import { fetchPaginate } from 'fetch-paginate';
-
-//https://registry.npmjs.org/-/v1/search?text=svelte&from=6400
-const { items } = await fetchPaginate('https://registry.npmjs.org/-/v1/search?text=skeletonlabs');
-console.dir(items);
